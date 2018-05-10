3fff85cc0cf8bd0942653d26621788e55da2113f
Andy <andy.rash1996@gmail.com>
2018-05-09 22:11:05 -0500

    consolidated phone_number column; modified __repr__ output accordingly

--- SemesterProject/src/models.py
+++ SemesterProject/src/models.py
@@ -1,25 +1,25 @@
 from sqlalchemy import (
-    Column, Integer, String
-)
+	Column, Integer, String
+	)
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Listing(Base):
-    '''
-    Class representing the table of telephone listings.
+	'''
+	Class representing the table of telephone listings.
 
-    '''
-    
-    __tablename__ = 'listings'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(255), nullable=False)
-    country_code = Column(Integer, nullable=False)
-    phone_number = Column(Integer, nullable=False)
+	'''
 
-    def __init__(self, **kwargs):
-        super(Listing, self).__init__(**kwargs)
+	__tablename__ = 'listings'
+	id = Column(Integer, primary_key=True, autoincrement=True)
+	name = Column(String(255), nullable=False, unique=True)
+	number_plan = Column(String(5), nullable=False)
+	phone_number = Column(String(16), nullable=False, unique=True)
 
-    def __repr__(self):
-        return f'<Listing {self.name}, {self.phone_number}>'
+	def __init__(self, **kwargs):
+		super(Listing, self).__init__(**kwargs)
+
+	def __repr__(self):
+		return f'<{self.name}, +{self.phone_number}>'
 
